93f2c2c63364d4ae25c7771619b2fd0b564bc1ca
sierikov <artem.sierikov@gmail.com>
2022-06-05 10:09:24 +0200

    Webpack Dev Server

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,10 @@ module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'main.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
+  },
+  mode: 'development',
+  devServer: {
+    static: './dist',
   }
-}
+};
