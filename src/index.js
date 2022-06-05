a371e96a11ab6c66662af3c1ee134e5abb66d58b
sierikov <artem.sierikov@gmail.com>
2022-06-05 12:53:12 +0200

    Complete

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-document.write("Hello asdsds!")
+import $ from 'jquery'
+import 'slick-carousel'
+
+import './sass/style.sass'
+
+window.$ = window.jQuery = $; // solve $ problem for jquery
+
+console.log("Loaded libs: ", $)
+
+$(".card_slider").slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: false,
+  autoplaySpeed: 5000,
+  dots: true,
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,53 @@
-const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+const webpack = require("webpack");
 
 module.exports = {
-  entry: './src/index.js',
+  entry: "./src/index.js",
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
   },
-  mode: 'development',
+  mode: "development",
   devServer: {
-    static: './dist',
-  }
+    static: "./dist",
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery",
+    }),
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader'
+          }
+        ]
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          "style-loader",
+          "css-loader",
+          {
+            loader: "sass-loader",
+            options: {
+              implementation: require("sass"),
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
 };
